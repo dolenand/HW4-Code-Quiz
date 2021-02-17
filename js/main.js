5b9c6f5ed66bf10e154388df3467c6bc3cb05804
Andy Dolen <dolenand@msu.edu>
2021-02-17 14:02:29 -0500

    Began declaring variables and questions prompts using an array in the QuizQuestions variable

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//Assign Variables and Test Questions
+
+var secondsLeft = 75;
+var ResultTime = 0;
+var pos = 0;
+var results = [];
+var resLength = 0;
+var timerInterval;
+var quizQuestions = 0 [
+    {
+        "question": "Blah Blah Blah:",
+        "answers": ["one", "two", "three"],
+        "right": "four",
+        "user": ""
+    },
+    {
+        "question": "Blah Blah Blah:",
+        "answers": ["one", "two", "three"],
+        "right": "four",
+        "user": ""
+    },
+    {
+        "question": "Blah Blah Blah:",
+        "answers": ["one", "two", "three"],
+        "right": "four",
+        "user": ""
+    },
+    {
+        "question": "Blah Blah Blah:",
+        "answers": ["one", "two", "three"],
+        "right": "four",
+        "user": ""
+    },
+    {
+        "question": "Blah Blah Blah:",
+        "answers": ["one", "two", "three"],
+        "right": "four",
+        "user": ""
+    },
+    {
+        "question": "Blah Blah Blah:",
+        "answers": ["one", "two", "three"],
+        "right": "four",
+        "user": ""
+    }
+];
+var quizUserReply = [];
+var quesNum = 0;
+
